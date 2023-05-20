cc4cb023e21f7e06ba90bc9e2761f534fdfeff2a
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-05-20 17:28:25 +0330

    Blogs Cards Component

---
+++ src/components/BlogCards.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+const BlogCards = (props) => {
+
+    const {image, date, title, desc} = props.blogs;
+    return (
+        <div className='col-3'>
+            <div className='blog-card'>
+                <div className='card-image'>
+                    <img src={image} alt="blog" />
+                </div>
+                <div className='blog-content'>
+                    <p className='date'>{date}</p>
+                    <h4 className='title'>{title}</h4>
+                    <p className='desc'>{desc}</p>
+                    <Link to="/" className='button'>Read More</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default BlogCards
