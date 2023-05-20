3b7e48c0189f7218ea635df89f0cba85ad6c5b3d
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-05-20 15:40:47 +0330

    Marquee for Brands

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// Section of Importing Difrents Libraris
+import Marquee from "react-fast-marquee";
+
 function Home() {
   return (
     <>
@@ -297,6 +300,42 @@ function Home() {
                     alt="categories"
                   />
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* section fore */}
+      <section className="marque-wrapper py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="marquee-inner-wrapper card-wrapper">
+                <Marquee className="d-flex">
+                  <div className="mx-4 w-25">
+                    <img src="images/brands/brand-01.png" alt="brand" />
+                  </div>
+
+                  <div className="mx-4 w-25">
+                    <img src="images/brands/brand-02.png" alt="brand" />
+                  </div>
+
+                  <div className="mx-4 w-25">
+                    <img src="images/brands/brand-03.png" alt="brand" />
+                  </div>
+
+                  <div className="mx-4 w-25">
+                    <img src="images/brands/brand-04.png" alt="brand" />
+                  </div>
+
+                  <div className="mx-4 w-25">
+                    <img src="images/brands/brand-05.png" alt="brand" />
+                  </div>
+
+                  <div className="mx-4 w-25">
+                    <img src="images/brands/brand-06.png" alt="brand" />
+                  </div>
+                </Marquee>
               </div>
             </div>
           </div>
