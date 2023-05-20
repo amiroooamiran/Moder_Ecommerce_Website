119237691b5e378fe869dfd6f97283d70e732e3a
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-05-20 17:28:35 +0330

    Blogs DataList

---
+++ src/data/BlogData.js
@@ -0,0 +1,26 @@
+export const blogData = [
+    {
+        image: 'images/blogs/blog-01.jpg',
+        date: '11 June 2023',
+        title: 'A Beautifull Sunday NEWS',
+        desc: 'It may come as a surprise to you that since the arrival of the PC you no longer need two.'
+    },
+    {
+        image: 'images/blogs/blog-02.jpg',
+        date: '21 Apr 2023',
+        title: 'A Beautifull Sunday NEWS',
+        desc: 'It may come as a surprise to you that since the arrival of the PC you no longer need two.'
+    },
+    {
+        image: 'images/blogs/blog-03.jpg',
+        date: '18 June 2023',
+        title: 'A Beautifull Sunday NEWS',
+        desc: 'It may come as a surprise to you that since the arrival of the PC you no longer need two.'
+    },
+    {
+        image: 'images/blogs/blog-04.jpg',
+        date: '1 May 2023',
+        title: 'A Beautifull Sunday NEWS',
+        desc: 'It may come as a surprise to you that since the arrival of the PC you no longer need two.'
+    },
+  ]
