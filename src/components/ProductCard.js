046fe68c48550f1d7f741fc56f3dbb091227aa73
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2023-07-23 03:26:18 +0330

    create product cards

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,10 +1,11 @@
 import ReactStars from "react-rating-stars-component";
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const ProductCard = () => {
   return (
     <div className='col-3'>
-        <div className='product-card posation-relative'>
+        <div className='product-card position-relative'>
             <div className='product-image'>
                 <img src='images/products/AirPodsPro.jpg' alt='product imags' />
             </div>
@@ -13,10 +14,26 @@ const ProductCard = () => {
                 <h5 className="product-title">
                 aesthetic video gym girl video motorcycle couple video sky.
                 </h5>
+                <ReactStars count={5} size={24} activeColor="#ffd700"/>
+                <p className='price'>$100.00</p>
             </div>
-            <ReactStars count={5} size={24} activeColor="#ffd700"/>
-            <p className='price'>$100.00</p>
 
+            <div className="action-bar position-absolute">
+                <div className="d-flex flex-column">
+                    <Link>
+                    <i class="uil uil-eye"></i>
+                    </Link>
+                    <Link>
+                        <i class="uil uil-share-alt"></i>
+                    </Link>
+                    <Link>
+                        <i class="uil uil-shopping-bag"></i>
+                    </Link>
+                </div>
+            </div>
+            
+
+           
 
         </div>
     </div>
